8387bad0adcd72b3c099c3e41067882cd4c319b9
gunesevitan <gunesevitan@gmail.com>
2023-08-14 21:21:01 +0300

    transforms

---
+++ src/2d_classification/torch_transforms.py
@@ -0,0 +1,72 @@
+import cv2
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+
+
+def get_classification_transforms(**transform_parameters):
+
+    """
+    Get transforms for classification dataset
+
+    Parameters
+    ----------
+    transform_parameters: dict
+        Dictionary of transform parameters
+
+    Returns
+    -------
+    transforms: dict
+        Transforms for training, validation and test sets
+    """
+
+    train_transforms = A.Compose([
+        A.Resize(
+            height=transform_parameters['resize_height'],
+            width=transform_parameters['resize_width'],
+            interpolation=cv2.INTER_NEAREST,
+            always_apply=True
+        ),
+        A.HorizontalFlip(p=transform_parameters['horizontal_flip_probability']),
+        A.Normalize(
+            mean=transform_parameters['normalize_mean'],
+            std=transform_parameters['normalize_std'],
+            max_pixel_value=transform_parameters['normalize_max_pixel_value'],
+            always_apply=True
+        ),
+        ToTensorV2(always_apply=True)
+    ])
+
+    val_transforms = A.Compose([
+        A.Resize(
+            height=transform_parameters['resize_height'],
+            width=transform_parameters['resize_width'],
+            interpolation=cv2.INTER_NEAREST,
+            always_apply=True
+        ),
+        A.Normalize(
+            mean=transform_parameters['normalize_mean'],
+            std=transform_parameters['normalize_std'],
+            max_pixel_value=transform_parameters['normalize_max_pixel_value'],
+            always_apply=True
+        ),
+        ToTensorV2(always_apply=True)
+    ])
+
+    test_transforms = A.Compose([
+        A.Resize(
+            height=transform_parameters['resize_height'],
+            width=transform_parameters['resize_width'],
+            interpolation=cv2.INTER_NEAREST,
+            always_apply=True
+        ),
+        A.Normalize(
+            mean=transform_parameters['normalize_mean'],
+            std=transform_parameters['normalize_std'],
+            max_pixel_value=transform_parameters['normalize_max_pixel_value'],
+            always_apply=True
+        ),
+        ToTensorV2(always_apply=True)
+    ])
+
+    classification_transforms = {'train': train_transforms, 'val': val_transforms, 'test': test_transforms}
+    return classification_transforms
