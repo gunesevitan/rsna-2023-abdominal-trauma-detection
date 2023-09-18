7f2cc56d233c930bf66ab1eff3f35fe33c8ebba4
gunesevitan <gunesevitan@gmail.com>
2023-09-18 11:11:02 +0300

    yolov8 trainer

---
+++ src/roi_detection/yolov8_trainer.py
@@ -0,0 +1,36 @@
+import sys
+import argparse
+import yaml
+from ultralytics import YOLO
+
+sys.path.append('..')
+import settings
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model_directory', type=str)
+    args = parser.parse_args()
+
+    model_directory = settings.MODELS / args.model_directory
+    hyp = yaml.load(open(model_directory / 'hyp.yaml', 'r'), Loader=yaml.FullLoader)
+
+    model_path = model_directory / 'yolov8s.pt'
+    model = YOLO(model_path)
+    data = model_directory / 'dataset.yaml'
+
+    results = model.train(
+        data=data,
+        epochs=50,
+        patience=20,
+        batch=16,
+        imgsz=512,
+        project=model_directory,
+        name='experiment',
+        exist_ok=False,
+        optimizer='AdamW',
+        seed=42,
+        deterministic=False,
+        **hyp
+    )
