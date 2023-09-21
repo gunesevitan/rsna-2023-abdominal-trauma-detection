0ca2e1387b488fda4ae37d03135f9da1c043a357
gunesevitan <gunesevitan@gmail.com>
2023-09-21 10:54:36 +0300

    yolov8 trainer

--- src/roi_detection/yolov8_trainer.py
+++ src/roi_detection/yolov8_trainer.py
@@ -16,14 +16,14 @@ if __name__ == '__main__':
     model_directory = settings.MODELS / args.model_directory
     hyp = yaml.load(open(model_directory / 'hyp.yaml', 'r'), Loader=yaml.FullLoader)
 
-    model_path = model_directory / 'yolov8s.pt'
+    model_path = model_directory / 'yolov8n.pt'
     model = YOLO(model_path)
     data = model_directory / 'dataset.yaml'
 
     results = model.train(
         data=data,
-        epochs=50,
-        patience=20,
+        epochs=100,
+        patience=100,
         batch=16,
         imgsz=512,
         project=model_directory,
