c4beb7255cd9e85b88df7c89fe8fd7feef9facbc
gunesevitan <gunesevitan@gmail.com>
2023-09-22 21:24:55 +0300

    monai models

--- src/3d_classification/monai_models.py
+++ src/3d_classification/monai_models.py
@@ -1,37 +1,16 @@
 import torch.nn as nn
 import monai.networks.nets
 
-
-class ClassificationHead(nn.Module):
-
-    def __init__(self, input_dimensions):
-
-        super(ClassificationHead, self).__init__()
-
-        self.bowel_head = nn.Linear(input_dimensions, 1, bias=True)
-        self.extravasation_head = nn.Linear(input_dimensions, 1, bias=True)
-        self.kidney_head = nn.Linear(input_dimensions, 3, bias=True)
-        self.liver_head = nn.Linear(input_dimensions, 3, bias=True)
-        self.spleen_head = nn.Linear(input_dimensions, 3, bias=True)
-
-    def forward(self, x):
-
-        bowel_output = self.bowel_head(x)
-        extravasation_output = self.extravasation_head(x)
-        kidney_output = self.kidney_head(x)
-        liver_output = self.liver_head(x)
-        spleen_output = self.spleen_head(x)
-
-        return bowel_output, extravasation_output, kidney_output, liver_output, spleen_output
+from heads import ClassificationHead
 
 
 class MONAIConvolutionalClassificationModel(nn.Module):
 
-    def __init__(self, model_class, model_args, dropout_rate, freeze_parameters):
+    def __init__(self, backbone_class, backbone_args, dropout_rate, freeze_parameters):
 
         super(MONAIConvolutionalClassificationModel, self).__init__()
 
-        self.backbone = getattr(monai.networks.nets, model_class)(**model_args)
+        self.backbone = getattr(monai.networks.nets, backbone_class)(**backbone_args)
 
         if freeze_parameters:
             for parameter in self.backbone.parameters():
@@ -46,6 +25,29 @@ class MONAIConvolutionalClassificationModel(nn.Module):
     def forward(self, x):
 
         x = self.backbone(x)
+        bowel_output, extravasation_output, kidney_output, liver_output, spleen_output = self.head(x)
+
+        return bowel_output, extravasation_output, kidney_output, liver_output, spleen_output
+
+
+class MONAITransformerClassificationModel(nn.Module):
+
+    def __init__(self, backbone_class, backbone_args, freeze_parameters):
+
+        super(MONAITransformerClassificationModel, self).__init__()
+        self.backbone = getattr(monai.networks.nets, backbone_class)(**backbone_args)
+
+        if freeze_parameters:
+            for parameter in self.backbone.parameters():
+                parameter.requires_grad = False
+
+        input_features = self.backbone.classification_head[0].in_features
+        self.backbone.classification_head = nn.Identity()
+        self.head = ClassificationHead(input_dimensions=input_features)
+
+    def forward(self, x):
+
+        x = self.backbone(x)[0]
         bowel_output, extravasation_output, kidney_output, liver_output, spleen_output = self.head(x)
 
         return bowel_output, extravasation_output, kidney_output, liver_output, spleen_output
