0f1268b4bb6ae88ae4ff2b4fb9a9aebaf1c29a44
gunesevitan <gunesevitan@gmail.com>
2023-09-24 15:41:45 +0300

    scan writer

--- src/dataset_utilities/scan_writer.py
+++ src/dataset_utilities/scan_writer.py
@@ -3,8 +3,10 @@ import os
 from tqdm import tqdm
 import numpy as np
 import pandas as pd
-from scipy.ndimage import zoom
 import cv2
+import pydicom
+import torch
+from monai.transforms import Resize
 
 sys.path.append('..')
 import settings
@@ -12,53 +14,70 @@ import settings
 
 if __name__ == '__main__':
 
-    image_dataset_directory = settings.DATA / 'datasets' / '2d_windowed_400w_50c_1mm_isotropic' / 'images'
+    dicom_dataset_directory = settings.DATA / 'rsna-2023-abdominal-trauma-detection' / 'train_images'
+    image_dataset_directory = settings.DATA / 'datasets' / '2d_1w_raw_size' / 'images'
     patient_ids = sorted(os.listdir(image_dataset_directory), key=lambda filename: int(filename))
 
-    output_directory = settings.DATA / 'datasets' / '3d_windowed_400w_50c_96x256x256' / 'volumes'
+    output_directory = settings.DATA / 'datasets' / '3d_1w_cropped_96x256x256' / 'volumes'
     output_directory.mkdir(parents=True, exist_ok=True)
 
-    df_dicom_tags = pd.read_parquet(settings.DATA / 'rsna-2023-abdominal-trauma-detection' / 'train_dicom_tags.parquet')
-    settings.logger.info(f'DICOM Tags Dataset Shape: {df_dicom_tags.shape} - Memory Usage: {df_dicom_tags.memory_usage().sum() / 1024 ** 2:.2f} MB')
-
-    df_dicom_tags['scan_id'] = df_dicom_tags['path'].apply(lambda x: str(x).split('/')[2]).astype(np.uint64)
-    df_dicom_tags['patient_id'] = df_dicom_tags['path'].apply(lambda x: str(x).split('/')[1]).astype(np.uint64)
-    df_dicom_tags['slice_id'] = df_dicom_tags['path'].apply(lambda x: str(x).split('/')[3].split('.')[0]).astype(np.uint64)
-    df_dicom_tags['z_position'] = df_dicom_tags['ImagePositionPatient'].apply(lambda x: eval(x)[-1]).astype(np.float32)
-    df_dicom_tags.sort_values(by=['patient_id', 'scan_id', 'z_position'], ascending=[True, True, False], inplace=True)
-    df_dicom_tags.reset_index(drop=True, inplace=True)
-    df_dicom_tags['z_position_diff'] = df_dicom_tags.groupby(['patient_id', 'scan_id'])['z_position'].diff().round(2)
-    df_dicom_tags['slice_id_diff'] = df_dicom_tags.groupby(['patient_id', 'scan_id'])[['slice_id']].diff()
-    df_dicom_tags['image_path'] = df_dicom_tags['path'].apply(lambda x: str(image_dataset_directory) + x.split('images')[-1].split('.')[0] + '.png')
+    resize = Resize(spatial_size=(96, 256, 256))
 
     for patient_id in tqdm(patient_ids):
 
         patient_directory = image_dataset_directory / str(patient_id)
         patient_scans = sorted(os.listdir(patient_directory), key=lambda filename: int(filename))
 
-        for scan_id in tqdm(patient_scans):
+        for scan_id in patient_scans:
 
-            df_scan = df_dicom_tags.loc[df_dicom_tags['scan_id'] == int(scan_id)].reset_index(drop=True)
             scan_directory = patient_directory / str(scan_id)
-            file_names = df_scan['slice_id'].values
-            scan = np.array([cv2.imread(str(scan_directory / f'{file_name}.png'), -1) for file_name in file_names])
+            file_names = sorted(os.listdir(scan_directory), key=lambda x: int(str(x).split('.')[0]))
 
-            try:
-                patient_position = df_scan.loc[0, 'PatientPosition']
-            except AttributeError:
-                patient_position = None
+            z_positions = []
+            patient_positions = []
+            scan = []
 
+            for file_idx, file_name in enumerate(file_names, start=1):
+
+                dicom = pydicom.dcmread(str(dicom_dataset_directory / patient_id / scan_id / str(file_name.split('.')[0] + '.dcm')))
+                image = cv2.imread(str(scan_directory / file_name), -1)
+
+                scan.append(image)
+
+                try:
+                    patient_position = dicom.PatientPosition
+                except AttributeError:
+                    patient_position = 'FFS'
+
+                patient_positions.append(patient_position)
+
+                try:
+                    z_position = float(dicom.ImagePositionPatient[-1])
+                except AttributeError:
+                    z_position = file_idx * -1
+
+                z_positions.append(z_position)
+
+            scan = np.array(scan)
+
+            # Sort CT scan slices by head to feet
+            sorting_idx_z = np.argsort(z_positions)[::-1]
+            scan = scan[sorting_idx_z]
+
+            patient_position = pd.Series(patient_positions).value_counts().index[0]
             if patient_position is not None:
                 if patient_position == 'HFS':
                     # Flip x-axis if patient position is head first
                     scan = np.flip(scan, axis=2)
 
             # Find partial slices by calculating sum of all zero vertical lines
-            scan_all_zero_vertical_line_transitions = np.diff(np.all(scan == 0, axis=1).sum(axis=1))
-            # Heuristically mask very high and low transitions on z-axis and drop them
-            slices_with_all_zero_vertical_lines = (scan_all_zero_vertical_line_transitions > 5) | (scan_all_zero_vertical_line_transitions < -5)
-            slices_with_all_zero_vertical_lines = np.append(slices_with_all_zero_vertical_lines, slices_with_all_zero_vertical_lines[-1])
-            scan = scan[~slices_with_all_zero_vertical_lines]
+            if scan.shape[0] != 1:
+                scan_all_zero_vertical_line_transitions = np.diff(np.all(scan == 0, axis=1).sum(axis=1))
+                # Heuristically select high and low transitions on z-axis and drop them
+                slices_with_all_zero_vertical_lines = (scan_all_zero_vertical_line_transitions > 5) | (scan_all_zero_vertical_line_transitions < -5)
+                slices_with_all_zero_vertical_lines = np.append(slices_with_all_zero_vertical_lines, slices_with_all_zero_vertical_lines[-1])
+                scan = scan[~slices_with_all_zero_vertical_lines]
+                del scan_all_zero_vertical_line_transitions, slices_with_all_zero_vertical_lines
 
             # Crop non-zero slices along xz, yz and xy planes
             mmin = np.array((scan > 0).nonzero()).min(axis=1)
@@ -66,10 +85,10 @@ if __name__ == '__main__':
             scan = scan[
                 mmin[0]:mmax[0] + 1,
                 mmin[1]:mmax[1] + 1,
-                mmin[2]:mmax[2] + 1,
+                mmin[2]:mmax[2] + 1
             ]
 
-            zoom_factor = np.array([96, 256, 256]) / np.array(scan.shape)
-            scan = zoom(scan, zoom=zoom_factor, mode='nearest')
+            scan = resize(torch.from_numpy(np.expand_dims(scan, axis=0)))
+            scan = torch.squeeze(scan, dim=0).numpy().astype(np.uint8)
 
             np.save(output_directory / f'{patient_id}_{scan_id}.npy', scan)
